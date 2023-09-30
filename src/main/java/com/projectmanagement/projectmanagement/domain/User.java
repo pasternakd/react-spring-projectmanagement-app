73982c732e5507e63c72dd9ca98fd8e9932fa3b7
Dawid <dawidpasternak07@gmail.com>
2023-09-30 15:10:51 +0200

    feature/implement UserDetails interface

--- src/main/java/com/projectmanagement/projectmanagement/domain/User.java
+++ src/main/java/com/projectmanagement/projectmanagement/domain/User.java
@@ -1,10 +1,16 @@
 package com.projectmanagement.projectmanagement.domain;
 
 import javax.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,10 +21,39 @@ public class User {
         return id;
     }
 
+    @Override
     public String getUsername() {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new Authorities("ROLE_ENGINEER"));
+        return roles;
+    }
+
+    @Override
     public String getPassword() {
         return password;
     }
